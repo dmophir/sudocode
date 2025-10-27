d098425d8b170cf16d118275af783585c7f00c9d
Alex Ngai <alexander.s.ngai@gmail.com>
2025-10-27 11:39:33 -0700

    kanban overflow

--- frontend/src/components/ui/kanban/index.tsx
+++ frontend/src/components/ui/kanban/index.tsx
@@ -125,7 +125,7 @@ export type KanbanCardsProps = {
 }
 
 export const KanbanCards = ({ children, className }: KanbanCardsProps) => (
-  <div className={cn('flex flex-1 flex-col', className)}>{children}</div>
+  <div className={cn('flex flex-1 flex-col overflow-y-auto', className)}>{children}</div>
 )
 
 export type KanbanHeaderProps =
